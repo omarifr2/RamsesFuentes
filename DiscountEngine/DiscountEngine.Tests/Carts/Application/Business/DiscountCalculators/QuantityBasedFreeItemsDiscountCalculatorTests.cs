c749f78769d24878d56c3ef03107a6bd2704016b
Ramsés Fuentes <ramses.f_perez@outlook.com>
2025-06-09 18:24:04 -0600

    - Implemented quantity based free item discount. - Added tests for many cases: Complete discount, partial discount, promotion applied but no items to discount and not applied.

--- DiscountEngine/DiscountEngine.Tests/Carts/Application/Business/DiscountCalculators/QuantityBasedFreeItemsDiscountCalculatorTests.cs
+++ DiscountEngine/DiscountEngine.Tests/Carts/Application/Business/DiscountCalculators/QuantityBasedFreeItemsDiscountCalculatorTests.cs
@@ -5,17 +5,84 @@ namespace DiscountEngine.Tests.Carts.Application.Business.DiscountCalculators
 {
     public class QuantityBasedFreeItemsDiscountCalculatorTests : BaseDiscountCalculatorTests
     {
-        [Test]
-        public void QuantityBasedFreeItemsDiscountCalculatorTests_ShouldApplySingleDiscount()
+        [TestCase("Chicken Breast", 3, 2, "Buy 3 get 2 free on \"Chicken Breast\"", 15.78)]
+        [TestCase("Whole Wheat Pasta", 5, 1, "Buy 5 get 1 free on \"Whole Wheat Pasta\"", 2.39)]
+        public void QuantityBasedFreeItemsDiscountCalculatorTests_ShouldApplySingleDiscount(string productName, int requiredQuantity, int discountQuantity, string expectedDescription, decimal expectedAmount)
         {
+            // Arrange
             Cart cart = new Cart(TestCart.Items);
-            QuantityBasedFreeItemsDiscountCalculator sut = new QuantityBasedFreeItemsDiscountCalculator();
+            Product? product = TestCart.Items.First(item => item.Product.Name == productName).Product;
+            QuantityBasedFreeItemsDiscountCalculator sut = new QuantityBasedFreeItemsDiscountCalculator(product, requiredQuantity, discountQuantity);
+
+            // Act
             CartDiscount? discount = sut.CalculateDiscount(cart);
-            
+            if (discount != null) cart.Discounts.Add(discount);
+
+            // Assert
             Assert.IsNotNull(discount);
-            
-            cart.Discounts.Add(discount);
-            Assert.Equals(cart.Subtotal, TestCartSubtotal);
+            Assert.That(discount.Description, Is.EqualTo(expectedDescription));
+            Assert.That(discount.Amount, Is.EqualTo(expectedAmount));
+            Assert.That(TestCartSubtotal, Is.EqualTo(cart.Subtotal));
+            Assert.That(cart.Total, Is.EqualTo(TestCartSubtotal - expectedAmount));
+        }
+
+        [TestCase("Whole Milk", 3, 2, "Buy 3 get 2 free on \"Whole Milk\", only 1 items discounted", 1.79)]
+        [TestCase("Diapers Size M", 2, 2, "Buy 2 get 2 free on \"Diapers Size M\", only 1 items discounted", 15.49)]
+        public void QuantityBasedFreeItemsDiscountCalculatorTests_PartialDiscountApplied(string productName, int requiredQuantity, int discountQuantity, string expectedDescription, decimal expectedAmount)
+        {
+            // Arrange
+            Cart cart = new Cart(TestCart.Items);
+            Product? product = TestCart.Items.First(item => item.Product.Name == productName).Product;
+            QuantityBasedFreeItemsDiscountCalculator sut = new QuantityBasedFreeItemsDiscountCalculator(product, requiredQuantity, discountQuantity);
+
+            // Act
+            CartDiscount? discount = sut.CalculateDiscount(cart);
+            if (discount != null) cart.Discounts.Add(discount);
+
+            // Assert
+            Assert.IsNotNull(discount);
+            Assert.That(discount.Description, Is.EqualTo(expectedDescription));
+            Assert.That(discount.Amount, Is.EqualTo(expectedAmount));
+            Assert.That(TestCartSubtotal, Is.EqualTo(cart.Subtotal));
+            Assert.That(cart.Total, Is.EqualTo(TestCartSubtotal - expectedAmount));
+        }
+
+        [TestCase("Smartphone", 1, 1, "Buy 1 get 1 free on \"Smartphone\", no item to discount", 0)]
+        [TestCase("Baby Wipes", 2, 1, "Buy 2 get 1 free on \"Baby Wipes\", no item to discount", 0)]
+        public void QuantityBasedFreeItemsDiscountCalculatorTests_DiscountAppliedButNoItemsToDiscount(string productName, int requiredQuantity, int discountQuantity, string expectedDescription, decimal expectedAmount)
+        {
+            // Arrange
+            Cart cart = new Cart(TestCart.Items);
+            Product? product = TestCart.Items.First(item => item.Product.Name == productName).Product;
+            QuantityBasedFreeItemsDiscountCalculator sut = new QuantityBasedFreeItemsDiscountCalculator(product, requiredQuantity, discountQuantity);
+
+            // Act
+            CartDiscount? discount = sut.CalculateDiscount(cart);
+            if (discount != null) cart.Discounts.Add(discount);
+
+            // Assert
+            Assert.IsNotNull(discount);
+            Assert.That(discount.Description, Is.EqualTo(expectedDescription));
+            Assert.That(discount.Amount, Is.EqualTo(expectedAmount));
+            Assert.That(TestCartSubtotal, Is.EqualTo(cart.Subtotal));
+            Assert.That(cart.Total, Is.EqualTo(TestCartSubtotal - expectedAmount));
+        }
+
+        [TestCase("Ground Beef", 8, 1, "", 0)]
+        public void QuantityBasedFreeItemsDiscountCalculatorTests_NotEnoughArticlesToApplyDiscount(string productName, int requiredQuantity, int discountQuantity, string expectedDescription, decimal expectedAmount)
+        {
+            // Arrange
+            Cart cart = new Cart(TestCart.Items);
+            Product? product = TestCart.Items.First(item => item.Product.Name == productName).Product;
+            QuantityBasedFreeItemsDiscountCalculator sut = new QuantityBasedFreeItemsDiscountCalculator(product, requiredQuantity, discountQuantity);
+
+            // Act
+            CartDiscount? discount = sut.CalculateDiscount(cart);
+
+            // Assert
+            Assert.IsNull(discount);
+            Assert.That(TestCartSubtotal, Is.EqualTo(cart.Subtotal));
+            Assert.That(TestCartSubtotal, Is.EqualTo(cart.Total));
         }
     }
 }
--- DiscountEngine/DiscountEngine/Carts/Application/Business/DiscountCalculators/QuantityBasedFreeItemsDiscountCalculator.cs
+++ DiscountEngine/DiscountEngine/Carts/Application/Business/DiscountCalculators/QuantityBasedFreeItemsDiscountCalculator.cs
@@ -8,11 +8,23 @@ using System.Threading.Tasks;
 
 namespace DiscountEngine.Carts.Application.Business.DiscountCalculators
 {
-    public class QuantityBasedFreeItemsDiscountCalculator : IDiscountCalculator
+    public class QuantityBasedFreeItemsDiscountCalculator(Product product, int requiredQuantity, int discountQuantity) : IDiscountCalculator
     {
         public CartDiscount? CalculateDiscount(Cart cart)
         {
-            throw new NotImplementedException();
+            int productQuantity = cart.Items.First(item => item.Product.Id == product.Id).Quantity;
+            if (productQuantity < requiredQuantity) return null;
+
+            string description = $"Buy {requiredQuantity} get {discountQuantity} free on \"{product.Name}\"";
+
+            if (productQuantity >= requiredQuantity + discountQuantity) return new CartDiscount(description, product.Price * discountQuantity);
+
+            if (productQuantity == requiredQuantity) return new CartDiscount($"{description}, no item to discount", 0);
+
+            int partialDiscountQuantity = productQuantity - requiredQuantity;
+            return new CartDiscount($"{description}, only {partialDiscountQuantity} items discounted", product.Price * partialDiscountQuantity);
+
+
         }
     }
 }
